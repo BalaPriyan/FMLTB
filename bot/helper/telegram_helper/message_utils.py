8bff12be8821567b178e2634f23a1c96a2ab76ca
Balapriyan B <131746104+BalaPriyan@users.noreply.github.com>
2023-07-22 08:17:17 -0700

    Update message_utils.py

--- bot/helper/telegram_helper/message_utils.py
+++ bot/helper/telegram_helper/message_utils.py
@@ -44,6 +44,37 @@ async def sendMessage(message, text, buttons=None):
         return str(e)
 
 
+async def sendCustomMsg(chat_id, text, buttons=None, photo=None):
+    try:
+        if photo:
+            try:
+                if photo == 'IMAGES':
+                    photo = rchoice(config_dict['IMAGES'])
+                return await bot.send_photo(chat_id=chat_id, photo=photo, caption=text,
+                                                        reply_markup=buttons, disable_notification=True)
+            except IndexError:
+                pass
+            except (PhotoInvalidDimensions, WebpageCurlFailed, MediaEmpty):
+                des_dir = await download_image_url(photo)
+                await sendCustomMsg(chat_id, text, buttons, des_dir)
+                await aioremove(des_dir)
+                return
+            except Exception as e:
+                LOGGER.error(format_exc())
+        return await bot.send_message(chat_id=chat_id, text=text, disable_web_page_preview=True,
+                                                  disable_notification=True, reply_markup=buttons)
+    except FloodWait as f:
+        LOGGER.warning(str(f))
+        await sleep(f.value * 1.2)
+        return await sendCustomMsg(chat_id, text, buttons, photo)
+    except ReplyMarkupInvalid:
+        return await sendCustomMsg(chat_id, text, None, photo)
+    except Exception as e:
+        LOGGER.error(format_exc())
+        return str(e)
+
+
+
 async def editMessage(message, text, buttons=None):
     try:
         if message.media:
@@ -77,6 +108,39 @@ async def chat_info(channel_id):
         LOGGER.error(f"{e.NAME}: {e.MESSAGE} for {channel_id}")
         return None
 
+async def sendMultiMessage(chat_ids, text, buttons=None, photo=None):
+    msg_dict = {}
+    for channel_id in chat_ids.split():
+        chat = await chat_info(channel_id)
+        try:
+            if photo:
+                try:
+                    if photo == 'IMAGES':
+                        photo = rchoice(config_dict['IMAGES'])
+                    sent = await bot.send_photo(chat_id=chat.id, photo=photo, caption=text,
+                                                     reply_markup=buttons, disable_notification=True)
+                    msg_dict[chat.id] = sent
+                    continue
+                except IndexError:
+                    pass
+                except (PhotoInvalidDimensions, WebpageCurlFailed, MediaEmpty):
+                    des_dir = await download_image_url(photo)
+                    await sendMultiMessage(chat_ids, text, buttons, des_dir)
+                    await aioremove(des_dir)
+                    return
+                except Exception as e:
+                    LOGGER.error(str(e))
+            sent = await bot.send_message(chat_id=chat.id, text=text, disable_web_page_preview=True,
+                                               disable_notification=True, reply_markup=buttons)
+            msg_dict[chat.id] = sent
+        except FloodWait as f:
+            LOGGER.warning(str(f))
+            await sleep(f.value * 1.2)
+            return await sendMultiMessage(chat_ids, text, buttons, photo)
+        except Exception as e:
+            LOGGER.error(str(e))
+            return str(e)
+    return msg_dict
 
 async def sendFile(message, file, caption=None):
     try:
@@ -126,6 +190,12 @@ async def auto_delete_message(cmd_message=None, bot_message=None):
             await deleteMessage(cmd_message)
         if bot_message is not None:
             await deleteMessage(bot_message)
+
+async def delete_links(message):
+    if config_dict['DELETE_LINKS']:
+        if reply_to := message.reply_to_message:
+            await deleteMessage(reply_to)
+        await deleteMessage(message)
 
 
 async def delete_all_messages():
@@ -442,3 +512,15 @@ async def request_limiter(message=None, query=None):
             await query.answer("Oops, Spam detected! I will mute you for 69 seconds.", show_alert=True)
         else:
             await sendMessage(message, "Oops, Spam detected! I will mute you for 69 seconds.")
+
+async def BotPm_check(message, button=None):
+    try:
+        temp_msg = await message._client.send_message(chat_id=message.from_user.id, text='<b>Checking Access...</b>')
+        await temp_msg.delete()
+        return None, button
+    except Exception as e:
+        if button is None:
+            button = ButtonMaker()
+        _msg = "<i>You didn't START the bot in PM (Private)</i>"
+        button.ubutton("Start Bot Now", f"https://t.me/{bot_name}?start=start", 'header')
+        return _msg, button
