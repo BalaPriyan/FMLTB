e2815275e7cc65543a2d4524a0f3a3b60fc16b22
Balapriyan B <131746104+BalaPriyan@users.noreply.github.com>
2023-07-22 19:51:38 -0700

    Update mega_download_status.py

--- bot/helper/mirror_utils/status_utils/mega_download_status.py
+++ bot/helper/mirror_utils/status_utils/mega_download_status.py
@@ -1,6 +1,6 @@
 from mega import MegaApi
 
-from bot.helper.ext_utils.bot_utils import (MirrorStatus, get_readable_file_size,get_readable_time, EngineStatus)
+from bot.helper.ext_utils.bot_utils import MirrorStatus, get_readable_file_size,get_readable_time, EngineStatus
 
 engine_ = f"MegaSDK v{MegaApi('test').getVersion()}"
 
